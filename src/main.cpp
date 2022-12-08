e456181cb5d7aea85fdb6a24850a3a5827c0b21d
Kylie Benfield <kylie@benfield.design>
2022-12-07 19:42:10 -0700

    Adding ColorConverter Utility

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <GLFW/glfw3.h>
 
 #include "ArgoConfig.h"
+#include "Utilities/ColorConverter.h"
 
 //int main(int argc, char* argv[]) {
 //    std::cout << argv[0] << " - Version " << ARGO_VERSION_MAJOR
@@ -116,8 +117,8 @@ int main(int, char**)
     // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
     // - Read 'docs/FONTS.md' for more instructions and details.
     // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+    io.Fonts->AddFontDefault();
+    io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
@@ -126,8 +127,9 @@ int main(int, char**)
 
     // Our state
     bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+//    ImVec4 clear_color = ImVec4(0.29f, 0.25f, 0.56f, 1.00f);
+//    ImVec4 clear_color = ConvertColor({133, 208, 79, 1});
+    ImVec4 clear_color = ConvertColor(0x1f7fbf);
 
     // Main loop
     while (!glfwWindowShouldClose(window))
@@ -150,34 +152,17 @@ int main(int, char**)
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
-            static float f = 0.0f;
-            static int counter = 0;
-
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
             ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
+            ImGui::Dummy(ImVec2(0.0f, 80.0f));
+
+            ImGui::Text("Application Version %d.%d.%d", ARGO_VERSION_MAJOR, ARGO_VERSION_MINOR, ARGO_VERSION_PATCH);
             ImGui::End();
         }
 
