7d0e67ae2c3b7e92f83f25cca46663b8cec4f190
Kylie Benfield <kylie@benfield.design>
2022-12-26 20:45:16 -0700

    Adding ColorConverter Utility

---
+++ include/Utilities/ColorConverter.h
@@ -0,0 +1,57 @@
+#ifndef ARGOCPP_COLOR_CONVERTER_H
+#define ARGOCPP_COLOR_CONVERTER_H
+
+/**
+ * Generic color that can be used throughout Argo
+ */
+class CommonColor {
+public:
+    float red;
+    float green;
+    float blue;
+    float alpha = 1.0f;
+
+    /**
+     * Returns an ImVec4 for compatibility with Dear ImGui
+     * @return ImVec4
+     */
+    ImVec4 ToImVec4() const {
+        return ImVec4(
+                red,
+                green,
+                blue,
+                alpha
+        );
+    }
+};
+
+/**
+ * Creates a CommonColor from an RGBA object
+ *
+ * @param rgba - { red, green, blue, alpha }; red, green, blue values are between 0 and 255; alpha is a float between 0 and 1
+ * @return CommonColor
+ */
+CommonColor ConvertColor(CommonColor rgba) {
+    return {rgba.red / 255, rgba.green / 255, rgba.blue / 255, rgba.alpha};
+}
+
+/**
+ * Creates a CommonColor from a hexadecimal value with alpha
+ * @param hex - 0xRRGGBBAA
+ * @return CommonColor
+ */
+CommonColor ConvertColor(unsigned int hex) {
+    CommonColor rgba = CommonColor();
+
+    fprintf(stdout, "Color:  r: %f, g: %f, b: %f, a: %f", ((hex >> 24) & 0xFF) / 255.0, ((hex >> 16) & 0xFF) / 255.0,
+            ((hex >> 8) & 0xFF) / 255.0, ((hex) & 0xFF) / 255.0);
+
+    rgba.red = ((hex >> 24) & 0xFF) / 255.0;
+    rgba.green = ((hex >> 16) & 0xFF) / 255.0;
+    rgba.blue = ((hex >> 8) & 0xFF) / 255.0;
+    rgba.alpha = ((hex) & 0xFF) / 255.0;
+
+    return rgba;
+}
+
+#endif //ARGOCPP_COLOR_CONVERTER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,12 @@
-#include <iostream>
-
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+
 #include <stdio.h>
 #include <GLFW/glfw3.h>
 
 #include "ArgoConfig.h"
 #include "Utilities/ColorConverter.h"
-
-//int main(int argc, char* argv[]) {
-//    std::cout << argv[0] << " - Version " << ARGO_VERSION_MAJOR
-//    << "." << ARGO_VERSION_MINOR << "."
-//    << ARGO_VERSION_PATCH << std::endl;
-//
-//    GLFWwindow *window;
-//
-//    if (!glfwInit()) {
-//        fprintf(stderr, "Failed to initialize GLFW\n");
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    window = glfwCreateWindow(1024, 768, "New Window", NULL, NULL);
-//    if (!window) {
-//        fprintf(stderr, "Failed to open GLFW window\n");
-//        glfwTerminate();
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    while (!glfwWindowShouldClose(window)) {
-//
-//        glfwSwapBuffers(window);
-//        glfwPollEvents();
-//    }
-//
-//    glfwTerminate();
-//
-//    std::cin.get();
-//
-//}
 
 static void glfw_error_callback(int error, const char* description)
 {
@@ -70,13 +38,13 @@ int main(int, char**)
     // GL 3.0 + GLSL 130
     const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 
     // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
     if (window == NULL)
         return 1;
     glfwMakeContextCurrent(window);
@@ -87,7 +55,7 @@ int main(int, char**)
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;        // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
     //io.ConfigViewportsNoAutoMerge = true;
@@ -95,7 +63,7 @@ int main(int, char**)
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
+//    ImGui::StyleColorsLight();
 
     // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
     ImGuiStyle& style = ImGui::GetStyle();
@@ -109,27 +77,13 @@ int main(int, char**)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
     io.Fonts->AddFontDefault();
-    io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
+    io.Fonts->AddFontFromFileTTF(R"(c:\Windows\Fonts\segoeui.ttf)", 18.0f);
 
     // Our state
-    bool show_demo_window = true;
 //    ImVec4 clear_color = ImVec4(0.29f, 0.25f, 0.56f, 1.00f);
-//    ImVec4 clear_color = ConvertColor({133, 208, 79, 1});
-    ImVec4 clear_color = ConvertColor(0x1f7fbf);
+    ImVec4 clear_color = ConvertColor({133, 208, 79, 1}).ToImVec4();
+//    ImVec4 clear_color = ConvertColor(0x1f7fbfff).ToImVec4();
 
     // Main loop
     while (!glfwWindowShouldClose(window))
@@ -146,21 +100,20 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::ShowStyleEditor();
+
+            ImGui::Dummy(ImVec2(0.0f, 30.0f));
 
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
+            ImGui::Dummy(ImVec2(0.0f, 30.0f));
+
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
-            ImGui::Dummy(ImVec2(0.0f, 80.0f));
+            ImGui::Dummy(ImVec2(0.0f, 30.0f));
 
             ImGui::Text("Application Version %d.%d.%d", ARGO_VERSION_MAJOR, ARGO_VERSION_MINOR, ARGO_VERSION_PATCH);
             ImGui::End();
