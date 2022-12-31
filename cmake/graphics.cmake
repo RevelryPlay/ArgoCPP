4040532f21eddbe5834497b2d9586f55420e7bbd
Kylie Benfield <kylie@benfield.design>
2022-12-30 18:30:44 -0700

    Moving submodules

--- cmake/graphics.cmake
+++ cmake/graphics.cmake
@@ -1,28 +1,28 @@
 find_package(OpenGL 3 REQUIRED)
 
 # GLFW
-add_subdirectory(lib/GLFW)
-target_include_directories(external_libs INTERFACE lib/GLFW/include)
-target_link_directories(external_libs INTERFACE lib/GLFW/src)
+add_subdirectory(external/GLFW)
+target_include_directories(external_libs INTERFACE external/GLFW/include)
+target_link_directories(external_libs INTERFACE external/GLFW/src)
 
 #imgui
 add_library(imgui_glfw STATIC
-        lib/imgui/imgui.cpp
-        lib/imgui/imgui_draw.cpp
-        lib/imgui/imgui_demo.cpp
-        lib/imgui/imgui_widgets.cpp
-        lib/imgui/imgui_tables.cpp
-        lib/imgui/backends/imgui_impl_glfw.cpp
-        lib/imgui/backends/imgui_impl_opengl3.cpp
+        external/imgui/imgui.cpp
+        external/imgui/imgui_draw.cpp
+        external/imgui/imgui_demo.cpp
+        external/imgui/imgui_widgets.cpp
+        external/imgui/imgui_tables.cpp
+        external/imgui/backends/imgui_impl_glfw.cpp
+        external/imgui/backends/imgui_impl_opengl3.cpp
         )
 
 target_link_libraries(imgui_glfw PUBLIC glfw ${OPENGL_LIBRARIES})
 
 target_include_directories(imgui_glfw
         PUBLIC
-        lib/imgui
-        lib/imgui/backends
-        lib/imgui/examples
+        external/imgui
+        external/imgui/backends
+        external/imgui/examples
         )
 
 target_link_libraries(external_libs INTERFACE
