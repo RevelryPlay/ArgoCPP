75e93a984ef14d684cfaa2d0869506e500865df7
Kylie Benfield <kylie@benfield.design>
2024-03-29 17:06:21 -0600

    Adding entry points for OpenGL and Vulkan

---
+++ Graphics/Vulkan/Renderer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "../include/Renderer.hpp"
+
+namespace Argo::Graphics {
+    void Renderer::render() {
+        std::cout << "Rendering with Vulkan" << '\n';
+    }
+}
